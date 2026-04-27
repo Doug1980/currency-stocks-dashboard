3f82e6cf9b7658dcbe816ac08f70d3db8967ddf9
Doug1980 <douglassalazar1980@gmail.com>
2026-04-27 17:42:44 -0300

    feat: add page transition template with horizontal slide animation

---
+++ src/app/template.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Template({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <motion.div
+      initial={{ x: "100%", opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{
+        type: "tween",
+        ease: "easeOut",
+        duration: 1.2,
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
